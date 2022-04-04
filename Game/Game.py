31c6305070fee68cbed226083ab7227c48896f49
theDeveloperShoon <seancotter04@gmail.com>
2022-04-03 21:37:20 -0400

    Made Game.Game to setup the Game's directories based on os (I only have linux and windows)

---
+++ Game/Game.py
@@ -0,0 +1,31 @@
+import os
+
+
+class Game:
+    def __init__(self):
+        self.setup()
+
+    def setup(self):
+        myOS = os.name
+
+        if myOS == 'posix':
+            localAppDataPath = os.getenv('HOME')
+
+            if os.path.exists(localAppDataPath + "/.Pytris"):
+                gameDataPath = localAppDataPath + "/.Pytris"
+            else:
+                os.mkdir(localAppDataPath + "/.Pytris")
+                gameDataPath = localAppDataPath + "/.Pytris"
+                os.mkdir(gameDataPath + '/screenshots')
+
+        else:
+            localAppDataPath = os.getenv('LOCALAPPDATA')
+
+            if os.path.exists(localAppDataPath + "/Pytris"):
+                gameDataPath = localAppDataPath + "/Pytris"
+            else:
+                os.mkdir(localAppDataPath + "/Pytris")
+                gameDataPath = localAppDataPath + "/Pytris"
+                os.mkdir(gameDataPath + '/screenshots')
+
+        self.gameDataPath = gameDataPath
