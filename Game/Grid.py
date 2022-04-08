cff4425af61e33a336c01cbaf0db6b756c738264
Sean Cotter <seancotter04@hotmail.com>
2022-04-07 21:13:24 -0400

    Created a clear grid function for the grid, to empty out the grid everytime

--- Game/Grid.py
+++ Game/Grid.py
@@ -9,3 +9,14 @@ class Grid:
     def save(self):
         self.grid = self.displayGrid
         print(self.grid)
+
+    def clear_grid(self):
+        blankedGrid = []
+        for row in self.displayGrid:
+            clearedRow = []
+            for tile in row:
+                newTile = tile
+                newTile[2] = False
+                clearedRow.append(newTile)
+            blankedGrid.append(clearedRow)
+        self.displayGrid = blankedGrid
