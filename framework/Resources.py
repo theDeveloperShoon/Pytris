b1f3d729f86481a8bce105512541584f3e630427
theDeveloperShoon <seancotter04@gmail.com>
2022-10-25 17:35:17 -0400

    Added a few colors to resources

--- framework/Resources.py
+++ framework/Resources.py
@@ -6,7 +6,8 @@ EMPTY_TILE = pygame.image.load(assetsFolder + '/tile.png')
 TILE_FILLED = pygame.image.load(assetsFolder + '/tilefilled.png')
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-
+GRAY = (128, 128, 128)
+RED = (225, 32, 32)
 
 pygame.font.init()
 
